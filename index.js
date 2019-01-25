774d0120b63273a02eeed6fb0b0a16e36c49385f
adrioro <artistsvillage@me.com>
2019-01-25 08:32:43 -0500

    Third commit

--- index.js
+++ index.js
@@ -2,3 +2,7 @@
 $("#afterId").click(function () {
     $(this).after("<h3>This text is being added after the button.</h3>")
 })
+//.before
+$("#beforeId").click(function () {
+    $("p").before("This text was created to be placed before the paragrap")
+})
