9df07a742966f5bfdbc2f50f383da4818bb40fbb
adrioro <artistsvillage@me.com>
2019-01-25 09:32:19 -0500

    Fifth commit

--- index.js
+++ index.js
@@ -7,6 +7,11 @@ $("#beforeId").click(function () {
     $("p").before("This text was created to be placed before the paragrap")
 })
 //.insertAfter
+//Inserting list element at the end of list.
 $("#insertbeforeId").click(function () {
-    $("<li >Item 4</li>").insertAfter("li:last");
+    $("<li>Item 4</li>").insertAfter("li:last");
+});
+
+$("#insertbeforeId").click(function () {
+    $("li:first").insertAfter("li:last");
 });
